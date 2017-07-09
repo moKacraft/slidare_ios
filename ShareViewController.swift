798637f995186c986745c9aaeea96949374d912e
sophiedumont <sophie.dumont@epitech.eu>
2017-07-09 18:58:58 +0200

    all files

--- ShareViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ShareViewController.swift
-//  Slidare
-//
-//  Created by Sophie DUMONT on 09/07/2017.
-//  Copyright © 2017 Julien. All rights reserved.
-//
-
-import Foundation
--- Slidare/FacebookSdkObjectifC.m
+++
@@ -1,9 +0,0 @@
-//
-//  FacebookSdkObjectifC.m
-//  Slidare
-//
-//  Created by JulienSup on 04/06/16.
-//  Copyright © 2016 Julien. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
